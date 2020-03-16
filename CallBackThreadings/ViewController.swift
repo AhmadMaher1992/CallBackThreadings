ae5459943961dbe3ca0ede3c8198dd3e18719c63
Ahmed Maher <ahmadmahereisa@gmail.com>
2020-03-16 16:13:56 +0200

    Using Call back Closure Demo

--- CallBackThreadings/ViewController.swift
+++ CallBackThreadings/ViewController.swift
@@ -20,28 +20,57 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        getUsers()
-    }
-    
-    func getUsers(){
-        guard  let path = Bundle.main.path(forResource: "eisa", ofType: "json") else { return }
-        let url = URL(fileURLWithPath: path)
-        do {
-           let data = try Data(contentsOf: url)
-           let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-            guard let array = json as? [[String: Any]] else { return }
-            var names = [String]()
-            for user in array {
-                guard let name = user["name"] as? String else {
-                    return
+        getUsers(completion: {(success , response , error) in
+            if success {
+                print("GetUser Block Called")
+                guard let names = response as? [String] else { return }
+                self.usersNames = names
+                self.tableView.reloadData()
+                print("Mission Done")
+            }else{
+                if let error = error {
+                    print(error)
                 }
-                names.append(name)
             }
-            print(names)
-        } catch  {
-        print("There Is Error")
-        }
+            
+        })
+        
     }
+    // escaping mean all call back code run later after this fuction closed up
+    func getUsers(completion: @escaping (Bool , Any? , Error?)-> Void){
+        print("GetUser Function Start")
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2){
+            guard  let path = Bundle.main.path(forResource: "eisa", ofType: "json") else { return }
+            let url = URL(fileURLWithPath: path)
+            do {
+                let data = try Data(contentsOf: url)
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                guard let array = json as? [[String: Any]] else { return }
+                var names = [String]()
+                for user in array {
+                    guard let name = user["name"] as? String else {
+                        return
+                    }
+                    names.append(name)
+                    
+                }
+                DispatchQueue.main.async {
+                    print("Go From Back Ground Thread To Main Thread")
+                    completion(true , names , nil)
+                }
+                
+                
+            } catch  {
+                print("There Is Error")
+                DispatchQueue.main.async {
+                    completion(false , nil , error)
+                }
+                
+            }
+        }
+        print("Dispatch Finished")
+        }
+      
 
 
 }
